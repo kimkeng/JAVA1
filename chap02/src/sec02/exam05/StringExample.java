e00ce6ec7effa0de36274ba69fa8c331132f8acd
tiggersanta <tiggersanta@gmail.com>
2023-05-01 17:03:38 +0900

    first commit

---
+++ chap02/src/sec02/exam05/StringExample.java
@@ -0,0 +1,15 @@
+package sec02.exam05;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String name = "김경민";
+		String job = "프로그래머";
+		// 처음에 대문자가 들어가면 클래스이다.(String, System, Class)
+		//String은 큰따옴표 사용 ""
+		System.out.println(name);
+		System.out.println(job);
+		// .은 패키지
+	}
+
+}
